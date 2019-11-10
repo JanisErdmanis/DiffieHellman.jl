3afdd704a4dfda7cb8f8eae183cdb8e7aad9507c
akels <akels14@gmail.com>
2019-11-10 23:12:10 +0100

    improved tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
-#using Tests
-# Lets run DiffieHellman over network
+using Test
 
 using DiffieHellman
 using CryptoGroups
@@ -7,31 +6,28 @@ using CryptoSignatures
 
 using Sockets
 
-server = listen(2000)
-
-@sync begin
-    @async global serversocket = accept(server)
-    global slavesocket = connect(2000)
-end
-
 # For Simplicity let's choose the same group and the same kind of signatures for both sides. Generally it could be different and even user could choose the generator himself.
 G = CryptoGroups.MODP160Group()
 
 # master
 server = Signer(G)
 serversign(data) = DSASignature(hash(data),server)
+serverid = hash(server.pubkey)
 
 # slave
 slave = Signer(G)
 slavesign(data) = DSASignature(hash(data),slave)
 
-### Let's say that we want slave to connect only to a true master. Thus he owns a certificate and from master had obtained a valid master id.
-
-serverid = hash(server.pubkey)
+### Let's assume that maintainer had contacted the servre
 
 @sync begin
-    @async global keyserver = diffie(serversocket,serversign,(d,s)->verify(d,s,G),G)
-    @async global keyslave = hellman(slavesocket,slavesign,(d,s)->verify(d,s,G) && hash(s.pubkey)==serverid) # x==serverid
+    server = listen(2000)
+    @async global serversocket = accept(server)
+    global slavesocket = connect(2000)
 end
 
-@show keyslave==keyserver
+### Let's say that we want slave to connect only to a true master. Thus he owns a certificate and from master had obtained a valid master id.
+@async global keyserver = diffie(serversocket,serversign,(d,s)->verify(d,s,G),G)
+keyslave = hellman(slavesocket,slavesign,(d,s)->verify(d,s,G) && hash(s.pubkey)==serverid) # x==serverid
+
+@test keyslave==keyserver
